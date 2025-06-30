6cfef8bf51c45101ca942d1f2657cbcd832d9b25
Federico Paolinelli <fpaoline@redhat.com>
2025-06-30 12:52:27 +0200

    Kind setup: wait for the multus pods to be ready
    Signed-off-by: Federico Paolinelli <fpaoline@redhat.com>

--- clab/kind/frr-k8s/setup.sh
+++ clab/kind/frr-k8s/setup.sh
@@ -3,11 +3,14 @@
 MULTUS_VERSION=${MULTUS_VERSION:-"v4.2.1"}
 MVLAN_VERSION=${MVLAN_VERSION:-"v1.7.1"}
 kubectl apply -k $(dirname ${BASH_SOURCE[0]})
-sleep 2s
-kubectl -n frr-k8s-system wait --for=condition=Ready --all pods --timeout 300s
-
 kubectl apply -f https://raw.githubusercontent.com/k8snetworkplumbingwg/multus-cni/refs/tags/${MULTUS_VERSION}/deployments/multus-daemonset.yml
 
+sleep 2s
+echo "Waiting for frr-k8s-system pods to be ready"
+kubectl -n frr-k8s-system wait --for=condition=Ready --all pods --timeout 300s
+
+echo "Waiting for multus pods to be ready"
+kubectl -n kube-system wait --for=condition=Ready --all pods --timeout 300s
 
 TEMP_GOBIN=$(mktemp -d)
 GOBIN=$TEMP_GOBIN go install github.com/containernetworking/plugins/plugins/main/macvlan@${MVLAN_VERSION}
