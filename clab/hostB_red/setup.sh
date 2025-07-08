4ad8e53facade1047f04fd7da675724ab13edde8
Federico Paolinelli <fpaoline@redhat.com>
2025-07-08 10:06:25 +0200

    Clab: add ipv6 default gateway via the leaf
    Mirroring what we do with ipv4 and setting a default gw via the fabric.
    
    Signed-off-by: Federico Paolinelli <fpaoline@redhat.com>

--- clab/hostB_red/setup.sh
+++ clab/hostB_red/setup.sh
@@ -4,3 +4,7 @@
 # set the default gw via eth1
 ip r del default
 ip r add default via 192.169.20.1
+
+# set the IPv6 default gw via eth1
+ip -6 r del default
+ip -6 r add default via 2001:db8:169:20::1
