7dcc72c9c2e7da4b4ab4a8a49a1b4b5e90b6dc71
Federico Paolinelli <fpaoline@redhat.com>
2025-07-18 18:37:27 +0200

    Bump version: align the various scripts to OPENPE_VERSION
    Aligning all the scripts so the env variable is consistent.
    
    Signed-off-by: Federico Paolinelli <fpaoline@redhat.com>

--- hack/release/bumpversion.sh
+++ hack/release/bumpversion.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 set -e
 
-if [ -z "$PEROUTER_VERSION" ]; then
-    echo "must set the PEROUTER_VERSION environment variable"
+if [ -z "$OPENPE_VERSION" ]; then
+    echo "must set the OPENPE_VERSION environment variable"
     exit -1
 fi
 
-sed -i "s/newTag:.*$/newTag: v$PEROUTER_VERSION/" config/pods/kustomization.yaml
+sed -i "s/newTag:.*$/newTag: v$OPENPE_VERSION/" config/pods/kustomization.yaml
 
-sed -i "s/version:.*$/version: $PEROUTER_VERSION/" charts/openperouter/Chart.yaml
-sed -i "s/appVersion:.*$/appVersion: v$PEROUTER_VERSION/" charts/openperouter/Chart.yaml
-sed -i "s/version:.*$/version: $PEROUTER_VERSION/" charts/openperouter/charts/crds/Chart.yaml
-sed -i "s/appVersion:.*$/appVersion: v$PEROUTER_VERSION/" charts/openperouter/charts/crds/Chart.yaml
+sed -i "s/version:.*$/version: $OPENPE_VERSION/" charts/openperouter/Chart.yaml
+sed -i "s/appVersion:.*$/appVersion: v$OPENPE_VERSION/" charts/openperouter/Chart.yaml
+sed -i "s/version:.*$/version: $OPENPE_VERSION/" charts/openperouter/charts/crds/Chart.yaml
+sed -i "s/appVersion:.*$/appVersion: v$OPENPE_VERSION/" charts/openperouter/charts/crds/Chart.yaml
 helm dep update charts/openperouter
 
 # Update version in website main page
-sed -i "s/OpenPERouter Version .*/OpenPERouter Version $PEROUTER_VERSION/" website/content/_index.md
+sed -i "s/OpenPERouter Version .*/OpenPERouter Version $OPENPE_VERSION/" website/content/_index.md
 
 # Update version references in installation page
-sed -i "s|openperouter/openperouter/.*/config/all-in-one/openpe.yaml|openperouter/openperouter/v$PEROUTER_VERSION/config/all-in-one/openpe.yaml|g" website/content/docs/installation.md
-sed -i "s|openperouter/openperouter/.*/config/all-in-one/crio.yaml|openperouter/openperouter/v$PEROUTER_VERSION/config/all-in-one/crio.yaml|g" website/content/docs/installation.md
-sed -i "s|github.com/openperouter/openperouter/config/default?ref=.*|github.com/openperouter/openperouter/config/default?ref=v$PEROUTER_VERSION|g" website/content/docs/installation.md
-sed -i "s|github.com/openperouter/openperouter/config/crio?ref=.*|github.com/openperouter/openperouter/config/crio?ref=v$PEROUTER_VERSION|g" website/content/docs/installation.md
+sed -i "s|openperouter/openperouter/.*/config/all-in-one/openpe.yaml|openperouter/openperouter/v$OPENPE_VERSION/config/all-in-one/openpe.yaml|g" website/content/docs/installation.md
+sed -i "s|openperouter/openperouter/.*/config/all-in-one/crio.yaml|openperouter/openperouter/v$OPENPE_VERSION/config/all-in-one/crio.yaml|g" website/content/docs/installation.md
+sed -i "s|github.com/openperouter/openperouter/config/default?ref=.*|github.com/openperouter/openperouter/config/default?ref=v$OPENPE_VERSION|g" website/content/docs/installation.md
+sed -i "s|github.com/openperouter/openperouter/config/crio?ref=.*|github.com/openperouter/openperouter/config/crio?ref=v$OPENPE_VERSION|g" website/content/docs/installation.md
