5cb710c05d2d617bd3c9a9382fc5f780294448e9
Miguel Duarte Barroso <mdbarroso@redhat.com>
2025-10-16 11:43:00 +0100

    cclm, kubevirt, multi-cluster: enable the FG
    Cross cluster live migration requires both KubeVirt clusters to have the
    `DecentralizedLiveMigration` feature gate enabled. This commit does that
    for both KinD clusters.
    
    Signed-off-by: Miguel Duarte Barroso <mdbarroso@redhat.com>

--- examples/evpn/multi-cluster/prepare.sh
+++ examples/evpn/multi-cluster/prepare.sh
@@ -13,6 +13,8 @@ kubectl apply -f https://github.com/kubevirt/kubevirt/releases/download/v1.6.2/k
 kubectl apply -f https://github.com/kubevirt/kubevirt/releases/download/v1.6.2/kubevirt-cr.yaml
 # Patch KubeVirt to allow scheduling on control-planes, so we can test live migration between two nodes
 kubectl patch -n kubevirt kubevirt kubevirt --type merge --patch '{"spec": {"workloads": {"nodePlacement": {"tolerations": [{"key": "node-role.kubernetes.io/control-plane", "operator": "Exists", "effect": "NoSchedule"}]}}}}'
+# Enable the decentralized live migration feature gate (requirement for cross cluster live migration)
+kubectl patch -n kubevirt kubevirt kubevirt --type merge --patch '{"spec": {"configuration": {"developerConfiguration": {"featureGates": [ "DecentralizedLiveMigration" ]}}}}'
 kubectl wait --for=condition=Available kubevirt/kubevirt -n kubevirt --timeout=10m
 
 # provision the manifests on the first cluster
@@ -24,6 +26,8 @@ kubectl apply -f https://github.com/kubevirt/kubevirt/releases/download/v1.6.2/k
 kubectl apply -f https://github.com/kubevirt/kubevirt/releases/download/v1.6.2/kubevirt-cr.yaml
 # Patch KubeVirt to allow scheduling on control-planes, so we can test live migration between two nodes
 kubectl patch -n kubevirt kubevirt kubevirt --type merge --patch '{"spec": {"workloads": {"nodePlacement": {"tolerations": [{"key": "node-role.kubernetes.io/control-plane", "operator": "Exists", "effect": "NoSchedule"}]}}}}'
+# Enable the decentralized live migration feature gate (requirement for cross cluster live migration)
+kubectl patch -n kubevirt kubevirt kubevirt --type merge --patch '{"spec": {"configuration": {"developerConfiguration": {"featureGates": [ "DecentralizedLiveMigration" ]}}}}'
 kubectl wait --for=condition=Available kubevirt/kubevirt -n kubevirt --timeout=10m
 
 apply_manifests_with_retries cluster-b-openpe.yaml cluster-b-workload.yaml
